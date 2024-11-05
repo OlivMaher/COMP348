b67b99084c2a3ede3891778b4b94545e723bfee5
OlivMaher <olimaherlol@gmail.com>
2024-11-05 14:01:29 -0500

    Implmeneted Options 1, 2, 3

--- A2/game.py
+++ A2/game.py
@@ -1,5 +1,7 @@
 #Contain code for UI (menu, various prompts, error message, and display final score)
 from grid import *
+import time
+import os
 
 def menu(grid):
     print("--------------------")
@@ -13,21 +15,49 @@ def menu(grid):
     print("4. New game")
     print("5. Exit")
 
-def selectTwo():
-    #Not case sensitive
-    #The cells will be displayed for 2 seconds if incorrect
-    #If the same value they will switch to the pairs
+def selectTwo(grid):
     cell1 = input("Enter cell coordinates (e.g., a0): ")
+    column1 = ord(cell1[:-1].lower()) - ord('a')
+    row1 = int(cell1[1:])
     cell2 = input("Enter cell coordinates (e.g., a0): ")
+    column2 = ord(cell2[:-1].lower()) - ord('a')
+    row2 = int(cell2[1:])
 
-def uncoverOne():
-    #Will uncover this cell
+    valueOfCell1 = grid.revealCell(column1, row1)
+    valueOfCell2 = grid.revealCell(column2, row2)
+    if valueOfCell1 != valueOfCell2:
+        menu(grid)
+        time.sleep(2)
+        grid.hideCell(column1,row1)
+        grid.hideCell(column2,row2)
+
+def uncoverOne(grid):
     cell = input("Enter cell coordinates (e.g., a0): ")
+    column = ord(cell[:-1].lower()) - ord('a')
+    row = int(cell[1:])
+    grid.revealCell(column,row)
+    
+def uncoverAll(grid):
+    grid.revealAll()
 
 def main():
     grid = Grid(4)
     menu(grid)
+    selection = int(input("Select: "))
 
+    while selection != 5:
+        if selection == 1:
+            selectTwo(grid)
+        elif selection == 2:
+            uncoverOne(grid)
+        elif selection == 3:
+            uncoverAll(grid)
+        elif selection == 4:
+            print(grid.newGrid())
+        
+        os.system("cls")
+        menu(grid)
+        selection = int(input("Select: "))
 
 if __name__ == "__main__":
     main()
--- A2/grid.py
+++ A2/grid.py
@@ -30,8 +30,32 @@ class Grid:
         return [[Cell(values.pop()) for _ in range(self.size)] for _ in range(self.size)]
     
     def displayGrid(self):
+        i = 0
         for row in self.cells:
-            print("     "+"    ".join(str(cell) for cell in row))
+            print("["+str(i)+"]".strip()+"   "+"    ".join(str(cell) for cell in row))
+            i += 1
         print()
+    
+    def revealCell(self,column,row):
+        cell = self.cells[row][column]
+        cell.reveal()
+        return cell.value
+    
+    def hideCell(self, column, row):
+        cell = self.cells[row][column]
+        cell.hide()
+        
+    def revealAll(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                self.cells[i][j].reveal()
+    
+    def newGrid(self):
+        numberOfPairs = ((self.size**2) //2)
+        values = [i for i in range(0, numberOfPairs)] * 2
+        random.shuffle(values)
+
+        return [[Cell(values.pop()) for _ in range(self.size)] for _ in range(self.size)]
+
 
 
